d0358fcadd1eb770b81650ba296536a0ad0112a1
henidammak <henidammak98@gmail.com>
2024-05-11 16:40:29 +0200

    Add IngeridentDAO and TailleDAO

---
+++ src/main/java/DAO/IngredientDAO.java
@@ -0,0 +1,91 @@
+package DAO;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import modele.Ingredient;
+
+
+public class IngredientDAO {
+	private SessionFactory sessionFactory;
+	
+	public IngredientDAO() {
+		sessionFactory = util.HibernateUtil.getSessionFactory();
+	}
+	
+	public boolean create(Ingredient i) {
+		Session session = sessionFactory.openSession();
+		Transaction tx = null;
+		boolean success = false;
+		try {
+			tx = session.beginTransaction();
+			session.persist(i);
+			tx.commit();
+			success = true;
+		} catch (Exception e) {
+			if (tx != null)
+				tx.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+		return success;
+	}
+	
+	public Ingredient findById(long id) {
+		Session session = sessionFactory.openSession();
+		Ingredient i = session.get(Ingredient.class, id);
+		session.close();
+		return i;
+	}
+	
+	public boolean update(Long id, String nom, float prix, int archive) {
+		Session session = sessionFactory.openSession();
+		Ingredient i = session.get(Ingredient.class, id);
+		boolean success = false;
+		if (i != null) {
+			i.setNom(nom);
+			i.setPrix(prix);
+			i.setArchive(archive);
+
+			Transaction tx = null;
+			try {
+				tx = session.beginTransaction();
+				session.persist(i);
+				tx.commit();
+				success = true;
+			} catch (Exception e) {
+				if (tx != null)
+					tx.rollback();
+				throw e;
+			} finally {
+				session.close();
+			}
+		}
+		return success;
+	}
+	
+	public boolean delete(long id) {
+		Session session = sessionFactory.openSession();
+		Ingredient i = session.get(Ingredient.class, id);
+		boolean success = false;
+		if (i != null) {
+			Transaction tx = null;
+			try {
+				tx = session.beginTransaction();
+				session.remove(i);
+				tx.commit();
+				success = true;
+			} catch (Exception e) {
+				if (tx != null)
+					tx.rollback();
+				throw e;
+			} finally {
+				session.close();
+			}
+		}
+		return success;
+	}
+
+}
---
+++ src/main/java/DAO/TaillepizzaDAO.java
@@ -0,0 +1,91 @@
+package DAO;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+
+import modele.Taillepizza;
+
+public class TaillepizzaDAO {
+	private SessionFactory sessionFactory;
+	
+	public TaillepizzaDAO() {
+		sessionFactory = util.HibernateUtil.getSessionFactory();
+	}
+	
+	public boolean create(Taillepizza tp) {
+		Session session = sessionFactory.openSession();
+		Transaction tx = null;
+		boolean success = false;
+		try {
+			tx = session.beginTransaction();
+			session.persist(tp);
+			tx.commit();
+			success = true;
+		} catch (Exception e) {
+			if (tx != null)
+				tx.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+		return success;
+	}
+	
+	public Taillepizza findById(long id) {
+		Session session = sessionFactory.openSession();
+		Taillepizza tp = session.get(Taillepizza.class, id);
+		session.close();
+		return tp;
+	}
+	
+	public boolean update(Long id, String taille, float prix, int archive) {
+		Session session = sessionFactory.openSession();
+		Taillepizza tp = session.get(Taillepizza.class, id);
+		boolean success = false;
+		if (tp != null) {
+			tp.setTaille(taille);
+			tp.setPrix(prix);
+			tp.setArchive(archive);
+
+			Transaction tx = null;
+			try {
+				tx = session.beginTransaction();
+				session.persist(tp);
+				tx.commit();
+				success = true;
+			} catch (Exception e) {
+				if (tx != null)
+					tx.rollback();
+				throw e;
+			} finally {
+				session.close();
+			}
+		}
+		return success;
+	}
+	
+	public boolean delete(long id) {
+		Session session = sessionFactory.openSession();
+		Taillepizza tp = session.get(Taillepizza.class, id);
+		boolean success = false;
+		if (tp != null) {
+			Transaction tx = null;
+			try {
+				tx = session.beginTransaction();
+				session.remove(tp);
+				tx.commit();
+				success = true;
+			} catch (Exception e) {
+				if (tx != null)
+					tx.rollback();
+				throw e;
+			} finally {
+				session.close();
+			}
+		}
+		return success;
+	}
+
+}
